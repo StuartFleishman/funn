7856c310bceb25880c9f1cdff345eb411a1139f2
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-09-26 22:21:34 -0400

    building product index page

--- src/pages/ProductShow.js
+++ src/pages/ProductShow.js
@@ -1,103 +1,32 @@
 import React from 'react'
 
 function ProductShow() {
-  return (
-    <div className="font-sans text-gray-900">
-
-      <main className="pt-10 pb-12 pl-20">
-        <button>
-    
-        </button>
-        <div className="flex">
-          <div className="flex flex-col w-1/2">
-            <div className="max-w-md">
-              <div className="flex items-center text-sm pt-9">
-                <span className="text-gray-900/40">T-shirt&nbsp;</span>
-                <span>/ Men's Collection</span>
-              </div>
-              <div className="pt-10">
-                <h1 className="text-4xl font-bold tracking-wide">Men's T-shirt</h1>
-              </div>
-              <div className="flex items-center justify-between pt-4">
-                <span className="text-3xl">$19.99</span>
-                <div className="flex items-center">
-                  <div className="flex space-x-px">
-                  
-                  </div>
-                  <div className="pl-2 leading-none">
-                    <span className="text-gray-900/40"> </span>
-                  </div>
-                </div>
-              </div>
-              <p className="pt-8 leading-relaxed">
-                Be kind to your body
-                and pleasant to look at. Also, you're beautiful.
-              </p>
-              <div className="flex pt-8 space-x-4">
-                <button
-                  className="w-5 h-5 rounded-full bg-[#58737d] hover:ring-4 hover:ring-gray-900/20"
-                />
-                <button
-                  className="w-5 h-5 rounded-full bg-[#58737d] hover:ring-4 hover:ring-gray-900/20"
-                />
-                <button
-                  className="w-5 h-5 rounded-full bg-[#545454] hover:ring-4 hover:ring-gray-900/20"
-                />
-                <button
-                  className="w-5 h-5 rounded-full bg-[#CBA5A5] hover:ring-4 hover:ring-gray-900/20"
-                />
-              </div>
-              <div className="flex space-x-6 pt-9">
-                <div className="flex items-center border rounded-sm border-gray-900/30">
-                  <button className="p-4" >
-              
-                  </button>
-                  <input
-                    type="text"
-                    className="w-16 h-full text-center outline-none"
-                  />
-                  <button className="p-4" >
-               
-                  </button>
-                </div>
-                <button
-                  className="py-4 text-sm font-bold text-black uppercase bg-teal-500 rounded-sm px-14 hover:bg-teal-600"
-                >
-                  Add to cart
-                </button>
-              </div>
-              <div className="pt-8">
-                Free 2-5 day shipping • Free returns • Funn
-              </div>
-              <div className="flex items-center justify-between pt-12">
-                <button
-                  className="flex items-center px-4 py-2 space-x-2 text-teal-500 hover:text-teal-600"
-                >
-              
-                  <span></span>
-                </button>
-                <div className="flex items-center space-x-6">
-           
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="relative flex flex-col items-end w-1/2">
-            <div className="flex items-start pr-20 space-x-2">
-              <span className="text-3xl font-bold leading-tight"></span>
-              <span className="text-xl"></span>
-            </div>
-            <div className="flex pr-10 space-x-16">
-              <button className="p-3"></button>
-              <button className="p-3"></button>
-            </div>
-            <img src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dCUyMHNoaXJ0c3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" />
-   
-          </div>
-        </div>
-      </main>
-    </div>
   
+  return (
+    <>
+    <div className="bg-black w-full h-2">
+
+    </div>
+    <div className="h-screen flex flex-col">
+      <div className="border-black border-r-4  flex flex-col w-1/5 h-screen">
+        <div className="m-2 text-xl flex justify-center underline">T-Shirts</div>
+        <div className="m-2 flex px-8">Men's</div>
+        <div className="m-2 flex px-8">Women's</div>
+        <div className="m-2 flex px-8">Kids</div>
+
+        <div className="m-2 py-2 text-xl flex justify-center underline">Soap</div>
+        <div className="m-2 flex px-8">Men's</div>
+        <div className="m-2 flex px-8">Women's</div>
+        <div className="m-2 flex px-8">Kids</div>
+
+        <div className="m-2 py-2 text-xl flex justify-center underline">Tapestry</div>
+        <div className="m-2 flex px-8">Men's</div>
+        <div className="m-2 flex px-8">Women's</div>
+        <div className="m-2 flex px-8">Kids</div>
+      </div>
+      
+    </div>
+    </>
   )
 }
 
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -25,21 +25,14 @@ function SignUp() {
 
 
   return (
-    <div className="container flex mx-auto max-w-screen-md h-screen">
-      <div className="mt-4 bg-blue-100 flex w-full h-screen text-center border-8 border-black rounded-xl ">
-        <form className="text-xl flex flex-col p-20 px-72" onSubmit={(e) => handleSubmit(e)}>
-          <h1 className="underline text-xl">Sign up!</h1>
-          <label className="m-2 text-center" id="name">Name</label>
-          <input value={userInput.name} onChange={(e) => setUserInput({...userInput, name: e.target.value})} type="text" />
-          <label className="m-2 text-center" id="email">Email</label>
-          <input value={userInput.email} onChange={(e) => setUserInput({...userInput, email: e.target.value})} type="email" />
-          <label className="m-2 text-center" id="password">Password</label>
-          <input value={userInput.password} onChange={(e) => setUserInput({...userInput, password: e.target.value})} type="password" />
-          <input className="m-4 text-xl rounded-lg bg-blue-800 text-white p-4" type="submit" value="Signup"></input>
-        </form>
-
-      </div>
+    
+    <div className="h-screen flex flex-wrap">
+  
+-
     </div>
+    
+ 
+  
   )
 }
 
